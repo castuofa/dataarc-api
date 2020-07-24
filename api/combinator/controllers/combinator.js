ab56ce577f852faf2cd420da539ba4d87d9a181d
Tim Sexton <tlsexto@gmail.com>
2020-07-23 20:55:48 -0500

    added auto event logging for data changes on create, update, delete

--- api/combinator/controllers/combinator.js
+++ api/combinator/controllers/combinator.js
@@ -1,8 +1,94 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services['combinator'].create(data, { files });
+    } else {
+      entity = await strapi.services['combinator'].create(ctx.request.body);
+    }
+    let entry = sanitizeEntity(entity, {
+      model: strapi.models['combinator'],
+    });
+
+    strapi.services.event.log(
+      'update',
+      strapi.models['combinator'].info.name,
+      entry.name,
+      ctx.state.user.id
+    );
+
+    return entry;
+  },
+
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services['combinator'].update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services['combinator'].update(
+        { id },
+        ctx.request.body
+      );
+    }
+
+    let entry = sanitizeEntity(entity, {
+      model: strapi.models['combinator'],
+    });
+
+    strapi.services.event.log(
+      'update',
+      strapi.models['combinator'].info.name,
+      entry.name,
+      ctx.state.user.id
+    );
+
+    return entry;
+  },
+
+  /**
+   * delete a record.
+   *
+   * @return {Object}
+   */
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services['combinator'].delete({ id });
+
+    let entry = sanitizeEntity(entity, {
+      model: strapi.models['combinator'],
+    });
+
+    strapi.services.event.log(
+      'update',
+      strapi.models['combinator'].info.name,
+      entry.name,
+      ctx.state.user.id
+    );
+
+    return entry;
+  },
+};
--- api/dataset-template/controllers/dataset-template.js
+++ api/dataset-template/controllers/dataset-template.js
@@ -1,8 +1,98 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services['dataset-template'].create(data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services['dataset-template'].create(
+        ctx.request.body
+      );
+    }
+    let entry = sanitizeEntity(entity, {
+      model: strapi.models['dataset-template'],
+    });
+
+    strapi.services.event.log(
+      'update',
+      strapi.models['dataset-template'].info.name,
+      entry.name,
+      ctx.state.user.id
+    );
+
+    return entry;
+  },
+
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services['dataset-template'].update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services['dataset-template'].update(
+        { id },
+        ctx.request.body
+      );
+    }
+
+    let entry = sanitizeEntity(entity, {
+      model: strapi.models['dataset-template'],
+    });
+
+    strapi.services.event.log(
+      'update',
+      strapi.models['dataset-template'].info.name,
+      entry.name,
+      ctx.state.user.id
+    );
+
+    return entry;
+  },
+
+  /**
+   * delete a record.
+   *
+   * @return {Object}
+   */
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services['dataset-template'].delete({ id });
+
+    let entry = sanitizeEntity(entity, {
+      model: strapi.models['dataset-template'],
+    });
+
+    strapi.services.event.log(
+      'update',
+      strapi.models['dataset-template'].info.name,
+      entry.name,
+      ctx.state.user.id
+    );
+
+    return entry;
+  },
+};
