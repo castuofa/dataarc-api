1ced63d1b601f2643dab202efb3c3bbb012935ad
Tim Sexton <tlsexto@gmail.com>
2020-07-27 16:32:57 -0500

    added seeding of permissions for objects, updated temporal coverage

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -12,17 +12,21 @@ var fs = require('fs');
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
+// load a file
+async function loadFile(name) {
+  const path = `../../${process.env.SEED_DATA}/${name}.json`;
+  try {
+    strapi.log.info(`Loading '${name}'`);
+    var content = fs.readFileSync(require.resolve(path));
+    return JSON.parse(content);
+  } catch (err) {
+    strapi.log.warn(`Missing '${name}'`);
+    return;
+  }
+}
+
 // Create the primary admin account
-async function bootstrap_admin() {
-  strapi.log.info(`(Bootstrap) Bootstrapping admin user`);
-
-  const admin = {
-    username: process.env.ADMIN_USERNAME || 'admin',
-    password: process.env.ADMIN_PASSWORD || 'admin',
-    email: process.env.ADMIN_EMAIL || 'admin@data-arc.org',
-    blocked: false,
-  };
-
+async function create_admin(admin) {
   const admin_orm = strapi.query('administrator', 'admin');
   const admins = await admin_orm.find({ username: admin.username });
 
@@ -34,88 +38,139 @@ async function bootstrap_admin() {
     try {
       await admin_orm.create(admin);
 
-      strapi.log.warn(`(Bootstrap) Primary admin account created`);
-    } catch (error) {
-      console.error(error);
+      strapi.log.info(`Primary admin account created`);
+    } catch (err) {
+      strapi.log.error(`${err}`);
     }
   } else {
-    strapi.log.warn(`(Bootstrap) Primary admin account already exists`);
+    strapi.log.warn(`Primary admin account already exists`);
   }
 }
 
-// Create the administrator role for users
-async function bootstrap_adminrole() {
-  strapi.log.info(`(Bootstrap) Bootstrapping user administrator role`);
+// create user roles
+async function create_roles(name) {
+  let roles = await loadFile(`data/${name}`);
 
-  const roles = await strapi
-    .query('role', 'users-permissions')
-    .find({ type: 'administrator', _limit: 1 });
-
-  if (roles.length === 0) {
-    try {
-      const plugins = await strapi.plugins[
-        'users-permissions'
-      ].services.userspermissions.getPlugins('en');
-
-      const permissions = await strapi.plugins[
-        'users-permissions'
-      ].services.userspermissions.getActions(plugins);
-
-      const admin_role = {
-        name: 'Administrator',
-        description: 'DataARC Administration Role',
-        permissions,
-        users: [],
-      };
-
-      await strapi.plugins[
-        'users-permissions'
-      ].services.userspermissions.createRole(admin_role);
-
-      strapi.log.warn(`(Bootstrap) Administrator role created`);
-    } catch (error) {
-      console.error(error);
+  for (let role of roles) {
+    // check if it exists
+    let existing = await strapi.query('role', 'users-permissions').find({
+      name: role.name,
+      _limit: 1,
+    });
+    if (existing.length === 0) {
+      let result = await strapi.query('role', 'users-permissions').create(role);
+      strapi.log.info(`Role '${role.name}' created ${JSON.stringify(result)}`);
+    } else {
+      strapi.log.warn(`Role '${role.name}' already exists`);
     }
-  } else {
-    strapi.log.warn(`(Bootstrap) Administrator role already exists`);
   }
 }
 
-// Load a resource
-async function bootstrap_resource(resource_type, resource_service) {
-  strapi.log.info(`(Bootstrap) Loading resource: ${resource_type}`);
+// seed a resource
+async function seed_data(name) {
+  let resources = await loadFile(`data/${name}`);
+  if (!resources) return;
 
-  const path = `../../${process.env.DATA_FOLDER}/${resource_type}.json`;
-
-  try {
-    require.resolve(path);
-  } catch (err) {
-    strapi.log.warn(`(Bootstrap) File not found: ${path}`);
-    return;
-  }
-
-  let resources = require(path);
+  let service = strapi.services[name];
 
   for (let resource of resources) {
-    if ((await resource_service.count(resource)) === 0) {
-      await resource_service.create(resource);
-      strapi.log.info(
-        `(Bootstrap) ${resource_type} created: ${JSON.stringify(resource)}`
-      );
-    } else {
-      strapi.log.info(
-        `(Bootstrap) ${resource_type} aready exist: ${JSON.stringify(resource)}`
-      );
+    if ((await service.count(resource)) === 0) {
+      await service.create(resource);
+      strapi.log.info(`${name} created: ${JSON.stringify(resource)}`);
     }
   }
+}
+
+// load seed permissions from file
+async function seed_permissions(name) {
+  let roles = await loadFile(`permissions/${name}`);
+  if (!roles) return;
+
+  for (let role of roles)
+    await set_permissions(role.name, role.type, name, role.actions);
+}
+
+// set user permissions for a controller
+async function set_permissions(role, type, controller, actions) {
+  // query for the role
+  const r = await strapi
+    .query('role', 'users-permissions')
+    .findOne({ type: role });
+
+  // make sure we found the role
+  if (!r) return;
+
+  // get target permissions for object
+  let p = await strapi.query('permission', 'users-permissions').find({
+    role: r.id,
+    type: type,
+    controller: controller,
+  });
+
+  // make sure we have permissions
+  if (p.length == 0) return;
+
+  strapi.log.info(`Setting '${role}' permissions for '${controller}'`);
+
+  // loop through the objects permissions setting everything
+  p.forEach((permission) => {
+    let enable = actions.includes(permission.action);
+    let p = permission;
+    p.enabled = enable;
+    strapi.query('permission', 'users-permissions').update({ id: p.id }, p);
+  });
+
+  return;
 }
 
 module.exports = async () => {
-  const seed_data = process.env.SEED_DATA || false;
+  const seed = process.env.SEED || false;
+  const admin = {
+    username: process.env.ADMIN_USERNAME || 'admin',
+    password: process.env.ADMIN_PASSWORD || 'admin',
+    email: process.env.ADMIN_EMAIL || 'admin@data-arc.org',
+    blocked: false,
+  };
 
-  if (seed_data) {
-    await bootstrap_admin();
-    await bootstrap_adminrole();
-    await bootstrap_resource('Categories', strapi.services.category);
+  if (seed) {
+    // create the admin account
+    await create_admin(admin);
+
+    // create the default user roles
+    await create_roles('role');
+
+    // seed the users
+    // await seed_data('user');
+
+    // seed the default data from the seed directory
+    // file names match controller names
+    await seed_data('category');
+    await seed_data('combinator');
+    await seed_data('combinator-query');
+    await seed_data('concept');
+    await seed_data('dataset');
+    await seed_data('dataset-feature');
+    await seed_data('dataset-field');
+    await seed_data('dataset-template');
+    await seed_data('event');
+    await seed_data('map-layer');
+    await seed_data('search');
+    await seed_data('temporal-coverage');
+    await seed_data('topic-map');
+
+    // now set the permissions
+    await seed_permissions('category');
+    await seed_permissions('combinator');
+    await seed_permissions('combinator-query');
+    await seed_permissions('concept');
+    await seed_permissions('dataset');
+    await seed_permissions('dataset-feature');
+    await seed_permissions('dataset-field');
+    await seed_permissions('dataset-template');
+    await seed_permissions('event');
+    await seed_permissions('map-layer');
+    await seed_permissions('search');
+    await seed_permissions('temporal-coverage');
+    await seed_permissions('topic-map');
   }
 };
