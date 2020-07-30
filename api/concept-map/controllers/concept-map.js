ae186700d718b8b9088cfe079e2f149f9c4fd002
Tim Sexton <tlsexto@gmail.com>
2020-07-30 12:31:35 -0500

    added xml-js library, updated resources to reflect connections

--- api/concept-map/controllers/concept-map.js
+++ api/concept-map/controllers/concept-map.js
@@ -91,4 +91,64 @@ module.exports = {
 
     return entry;
   },
+
+  /**
+   * process a concept map
+   *
+   * @return {Object}
+   */
+
+  async process(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services['concept-map'].findOne({ id });
+    let entry = sanitizeEntity(entity, { model: strapi.models['concept-map'] });
+
+    strapi.log.info(`Processing concept map ${JSON.stringify(entry)}`);
+
+    const convert = require('xml-js');
+    const fs = require('fs');
+
+    // read file
+    const xmlFile = fs.readFileSync(`../public${entry.url}`, 'utf8');
+
+    // parse xml file as a json object
+    const jsonData = JSON.parse(
+      convert.xml2json(xmlFile, { compact: true, spaces: 2 })
+    );
+
+    strapi.log.info(`JSON data: ${JSON.stringify(jsonData)}`);
+
+    // const targetNode =
+
+    //     // element '/stats/runs/latest'
+    //     jsonData.stats.runs.latest
+
+    //     .find(x =>
+
+    //         // attribute '@date'
+    //         x._attributes.date === '2019-12-12'
+    //     );
+
+    // targetNode has the 'latest' node we want
+    // now output the 'fail' attribute from that node
+    // console.log(targetNode._attributes.fail);  // outputs: 2
+
+    // load the file
+
+    // loop through topics and create new entries
+
+    // let entry = sanitizeEntity(entity, {
+    //   model: strapi.models['concept-map'],
+    // });
+
+    // strapi.services.event.log(
+    //   'process',
+    //   strapi.models['concept-map'].info.name,
+    //   entry.name,
+    //   ctx.state.user.id
+    // );
+
+    return entry;
+  },
 };
