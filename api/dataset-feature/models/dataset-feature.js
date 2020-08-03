0ae076f0d2c113219a1e35cbfb22de7263b58892
Tim Sexton <tlsexto@gmail.com>
2020-08-03 17:47:08 -0500

    auto generate "name" add "title" column, make required

--- api/dataset-feature/models/dataset-feature.js
+++ api/dataset-feature/models/dataset-feature.js
@@ -1,8 +1,21 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
+const slugify = require('slugify');
+const options = {
+  lower: true,
+  strict: true,
+  remove: /[*+~.()'"!:@]/g,
+};
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.title) {
+        data.name = slugify(data.title, options);
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      data.name = slugify(data.title, options);
+    },
+  },
+};
