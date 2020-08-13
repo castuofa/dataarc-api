16d88f462f905e2bdce5508a680a79105888f6f3
Tim Sexton <tlsexto@gmail.com>
2020-08-13 14:11:25 -0500

    all permission types are now handled and defined initially within role

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -7,53 +7,12 @@ async function loadFile(type, name) {
   const seed_path = `${strapi.dir}/${process.env.SEED_DATA}`;
   const file = `${seed_path}/${type}/${name}.json`;
   try {
-    strapi.log.info(`loading ${name} ${type}`);
     var content = fs.readFileSync(require.resolve(file));
+    strapi.log.info(`loading ${name} ${type}`);
     return JSON.parse(content);
   } catch (err) {
     strapi.log.warn(`missing ${name} ${type}`);
     return;
-  }
-}
-
-// create user roles
-async function seed_roles() {
-  let roles = await loadFile('data', 'role');
-
-  for (let role of roles) {
-    // check if it exists
-    let existing = await strapi.query('role', 'users-permissions').find({
-      name: role.name,
-      _limit: 1,
-    });
-    if (existing.length === 0) {
-      let result = await strapi.query('role', 'users-permissions').create(role);
-      strapi.log.info(`role created: ${role.name}`);
-    } else {
-      strapi.log.warn(`role exists: ${role.name}`);
-    }
-  }
-}
-
-// seed a resource
-async function seed_data(name) {
-  let resources = await loadFile('data', name);
-  if (!resources) return;
-
-  let service = strapi.query(name);
-
-  for (let resource of resources) {
-    const existing = await service.find(resource);
-    if (existing.length === 0) {
-      await service.create(resource);
-      strapi.log.info(
-        `${name} created: '${JSON.stringify(resource).substring(0, 35)}...'`
-      );
-    } else {
-      strapi.log.warn(
-        `${name} exists: '${JSON.stringify(resource).substring(0, 35)}...'`
-      );
-    }
   }
 }
 
@@ -90,7 +49,60 @@ async function set_permissions(role, type, controller, actions) {
   return;
 }
 
-// load seed permissions from file
+// create user roles
+async function seed_roles() {
+  let roles = await loadFile('data', 'role');
+
+  for (let role of roles) {
+    // check if it exists
+    let existing = await strapi.query('role', 'users-permissions').find({
+      name: role.name,
+      _limit: 1,
+    });
+    if (existing.length === 0) {
+      let result = await strapi.query('role', 'users-permissions').create(role);
+      strapi.log.info(`role created: ${role.name}`);
+    } else {
+      strapi.log.warn(`role exists: ${role.name}`);
+    }
+
+    // load seed permissions if they exist
+    if (Array.isArray(role.permissions) && role.permissions.length) {
+      for (let permission of role.permissions) {
+        await set_permissions(
+          role.type,
+          permission.type,
+          permission.controller,
+          permission.actions
+        );
+      }
+    }
+  }
+}
+
+// seed a resource
+async function seed_data(name) {
+  let resources = await loadFile('data', name);
+  if (!resources) return;
+
+  let service = strapi.query(name);
+
+  for (let resource of resources) {
+    const existing = await service.find(resource);
+    if (existing.length === 0) {
+      await service.create(resource);
+      strapi.log.info(
+        `${name} created: '${JSON.stringify(resource).substring(0, 35)}...'`
+      );
+    } else {
+      strapi.log.warn(
+        `${name} exists: '${JSON.stringify(resource).substring(0, 35)}...'`
+      );
+    }
+  }
+}
+
+// seed resource permissions
 async function seed_permissions(name) {
   let roles = await loadFile('permissions', name);
   if (!roles) return;
@@ -140,15 +152,12 @@ module.exports = async () => {
     'combinator-query',
     'concept',
     'dataset',
-    'dataset-feature',
-    'dataset-field',
-    'dataset-template',
     'event',
     'map-layer',
     'search',
     'temporal-coverage',
     'topic-map',
-    // 'user',
+    'topic',
   ];
   // only seed if true
   if (seed) {
