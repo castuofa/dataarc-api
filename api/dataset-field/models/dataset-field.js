a253f87b1401f75ae80ce76633a1b392c787862d
Tim Sexton <tlsexto@gmail.com>
2020-08-22 10:56:39 -0500

    Flag for refresh after field updates

--- api/dataset-field/models/dataset-field.js
+++ api/dataset-field/models/dataset-field.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const _ = require('lodash');
+
 module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
@@ -10,6 +12,26 @@ module.exports = {
     beforeUpdate: async (params, data) => {
       if (data.title && !data.name) {
         data.name = strapi.services.helper.get_name(data.title);
+      }
+    },
+    afterUpdate: async (result, params, data) => {
+      // watch for changes to specific fields and set state
+      let watch_for = ['type'];
+      if (_.intersection(_.keys(data), watch_for).length > 0) {
+        strapi.services.helper.set_state(
+          result.id,
+          'dataset',
+          'refresh',
+          'pending',
+          'Related fields have been updated, please refresh the dataset'
+        );
+        strapi.services.helper.set_state(
+          { dataset: result.dataset.id },
+          'combinator',
+          'refresh',
+          'pending',
+          'Related fields have been updated, please verify combinator settings'
+        );
       }
     },
   },
