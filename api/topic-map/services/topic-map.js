7ce93809bdc23a33897cf4455a164962a718b959
Tim Sexton <tlsexto@gmail.com>
2020-10-05 13:42:15 -0500

    Adjust topic map processing to include concept mapping

--- api/topic-map/services/topic-map.js
+++ api/topic-map/services/topic-map.js
@@ -13,21 +13,26 @@ module.exports = {
       });
 
       // read file
-      const fs = require('fs');
       const path = `${strapi.dir}/public${entry.source.url}`;
-      const source = JSON.parse(fs.readFileSync(path, 'utf8'));
+      const source = strapi.services.helper.load_json(path);
+
+      // get the nodes and edges
+      const nodes = source.nodes;
+      const edges = source.edges;
 
       // map nodes to the correct format
-      let topics = source.nodes.map((node) => {
+      let topics = nodes.map((node) => {
+        let concept = strapi.query('concept').findOne({ name: node.title });
         return {
           identifier: node.id.toString(),
-          title: node.title,
+          name: node.title,
+          concept: concept.id,
           topic_map: entry.id,
         };
       });
 
       // create new topics
-      strapi.log.info(`Creating ${source.nodes.length} topics`);
+      strapi.log.info(`Creating ${nodes.length} topics`);
       if (Array.isArray(topics))
         await Promise.all(topics.map(strapi.query('topic').create));
     }
