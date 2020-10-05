4b1e64b3f0cac41bac70c0e886a5f5022d8c57c1
Tim Sexton <tlsexto@gmail.com>
2020-10-05 13:57:22 -0500

    Add concept map lifecycles

--- api/concept-map/models/concept-map.js
+++ api/concept-map/models/concept-map.js
@@ -1,8 +1,52 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.title && !data.name)
+        data.name = await strapi.services.helper.find_unique({
+          content_type: 'concept-map',
+          field: 'name',
+          value: data.title,
+        });
+    },
+    beforeUpdate: async (params, data) => {
+      if (data.title && !data.name)
+        data.name = await strapi.services.helper.find_unique({
+          content_type: 'concept-map',
+          field: 'name',
+          value: data.title,
+        });
+    },
+    afterCreate: async (result, data) => {
+      if (result == null) return;
+      strapi.services.helper.log_event(
+        'create',
+        'concept-map',
+        result.name,
+        result.updated_by == null ? null : result.updated_by.id,
+        { data }
+      );
+    },
+    afterUpdate: async (result, params, data) => {
+      if (result == null) return;
+      strapi.services.helper.log_event(
+        'update',
+        'concept-map',
+        result.name,
+        result.updated_by == null ? null : result.updated_by.id,
+        { params, data }
+      );
+    },
+    afterDelete: async (result, params) => {
+      if (result == null) return;
+      strapi.services.helper.log_event(
+        'delete',
+        'concept-map',
+        result.name,
+        result.updated_by == null ? null : result.updated_by.id,
+        { params }
+      );
+    },
+  },
+};
