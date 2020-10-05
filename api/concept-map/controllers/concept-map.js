afa6a5c1c1039598a66928d8a61869d23150b64c
Tim Sexton <tlsexto@gmail.com>
2020-10-05 13:56:32 -0500

    Add routes, services, and functions for processing concept maps

--- api/concept-map/controllers/concept-map.js
+++ api/concept-map/controllers/concept-map.js
@@ -1,8 +1,24 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  process: async (ctx) => {
+    const { id } = ctx.params;
+
+    let entity = await strapi.services['concept-map'].process({ id });
+    let entry = sanitizeEntity(entity, {
+      model: strapi.models['concept-map'],
+    });
+
+    if (entry != null)
+      strapi.services.helper.log_event(
+        'process',
+        'concept-map',
+        entry.name,
+        typeof ctx.state.user !== 'undefined' ? ctx.state.user.id : null
+      );
+
+    return entry;
+  },
+};
