b55ba1c7ae892691de924dd583b376dc0f072797
Tim Sexton <tlsexto@gmail.com>
2020-10-21 17:37:56 -0500

    Add category settings

---
+++ api/category/config/schema.graphql.js
@@ -0,0 +1,33 @@
+module.exports = {
+  definition: /* GraphQL */ `
+    extend type Category {
+      datasets_count: Int
+    }
+  `,
+  query: `
+    countCategories(where: JSON): Int!
+  `,
+  resolver: {
+    Query: {
+      countCategories: {
+        description: 'Return the count of categories',
+        resolver: 'application::category.category.count',
+      },
+      categories: {
+        resolverOf: 'application::category.category.find',
+        resolver: async (obj, options, ctx) => {
+          const params = await strapi.services.helper.prefix_graphql_params(
+            options
+          );
+          const results = await strapi.query('category').find(params);
+          results.map((doc) => {
+            doc.datasets_count = strapi
+              .query('dataset')
+              .count({ category: doc.id });
+          });
+          return results;
+        },
+      },
+    },
+  },
+};
