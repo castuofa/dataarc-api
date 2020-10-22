58799eb9ebe9d4f3e2f7af63f44bca0a92c35221
Tim Sexton <tlsexto@gmail.com>
2020-10-22 11:24:38 -0500

    Fix topics_count in graphql query for concept-maps

--- api/concept-map/config/schema.graphql.js
+++ api/concept-map/config/schema.graphql.js
@@ -1,12 +1,30 @@
 module.exports = {
+  definition: /* GraphQL */ `
+    extend type ConceptMaps {
+      topics_count: Int
+    }
+  `,
   query: `
-    conceptMapsCount(where: JSON): Int!
+    countConceptMaps(where: JSON): Int!
   `,
   resolver: {
     Query: {
-      conceptMapsCount: {
+      countConceptMaps: {
         description: 'Return the count of concept maps',
         resolver: 'application::concept-map.concept-map.count',
+      },
+      combinators: {
+        resolverOf: 'application::concept-map.concept-map.find',
+        resolver: async (obj, options, ctx) => {
+          const params = await strapi.services.helper.prefix_graphql_params(
+            options
+          );
+          const results = await strapi.query('concept-map').find(params);
+          results.map((doc) => {
+            doc.topics_count = doc.topics.length;
+          });
+          return results;
+        },
       },
     },
   },
