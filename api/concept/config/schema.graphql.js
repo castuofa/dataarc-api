38bd8ee411168b6bb169afc0ef1582f7bed76458
Tim Sexton <tlsexto@gmail.com>
2020-10-21 19:15:45 -0500

    Remove unused counts from concept results

--- api/concept/config/schema.graphql.js
+++ api/concept/config/schema.graphql.js
@@ -1,10 +1,4 @@
 module.exports = {
-  definition: /* GraphQL */ `
-    extend type Concept {
-      related_count: Int
-      contextual_count: Int
-    }
-  `,
   query: `
     countConcepts(where: JSON): Int!
   `,
@@ -13,21 +7,6 @@ module.exports = {
       countConcepts: {
         description: 'Return the count of concepts',
         resolver: 'application::concept.concept.count',
-      },
-      concepts: {
-        resolverOf: 'application::concept.concept.find',
-        resolver: async (obj, options, ctx) => {
-          const params = await strapi.services.helper.prefix_graphql_params(
-            options
-          );
-          const results = await strapi.query('concept').find(params);
-          console.log(`${JSON.stringify(results, null, 2)}`);
-          results.map((doc) => {
-            doc.combinators_count = results.related.length;
-            doc.combinators_count = results.contextual.length;
-          });
-          return results;
-        },
       },
     },
   },
