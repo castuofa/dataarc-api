3c09fe32481f3f06de300b08258847cd514c80d6
Tim Sexton <tlsexto@gmail.com>
2020-10-24 03:51:44 -0500

    Add concept-link

---
+++ api/concept-link/models/concept-link.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let info = {
+  name: 'concept-link',
+  field: 'name',
+};
+
+module.exports = {
+  lifecycles: {
+    afterCreate: async (result, data) => {
+      strapi.services.event.lifecycle_create({ info, result, data });
+    },
+    afterUpdate: async (result, params, data) => {
+      strapi.services.event.lifecycle_update({ info, result, params, data });
+    },
+    afterDelete: async (result, params) => {
+      strapi.services.event.lifecycle_delete({ info, result, params });
+    },
+  },
+};
