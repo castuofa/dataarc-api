21f38ed52e239b8e92961a6e79991bc197927d0c
Tim Sexton <tlsexto@gmail.com>
2020-10-31 10:42:38 -0500

    Add error handling for combinator-query when marking for review

--- api/combinator-query/services/combinator-query.js
+++ api/combinator-query/services/combinator-query.js
@@ -4,6 +4,9 @@ module.exports = {
   markReviewByField: async (id) => {
     strapi
       .query('combinator-query')
-      .update({ dataset_field: id }, { review: true });
+      .update({ dataset_field: id }, { review: true })
+      .catch((e) => {
+        strapi.log.error(`Unable to update combinator-query: ${e.message}`);
+      });
   },
 };
