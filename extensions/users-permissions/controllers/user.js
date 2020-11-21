d277385ff498a3a7fbd06683fe42bb081cb352ba
Tim Sexton <tlsexto@gmail.com>
2020-11-21 13:57:13 -0600

    Add endpoint to update "me" object for user profiles

---
+++ extensions/users-permissions/controllers/user.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { sanitizeEntity } = require('strapi-utils');
+const { validateProfileUpdateInput } = require('./validation/authenticated-user');
+
+const sanitizeUser = user =>
+  sanitizeEntity(user, {
+    model: strapi.query('user', 'users-permissions').model,
+  });
+
+module.exports = {
+ /**
+   * Update authenticated user.
+   * @return {Object|Array}
+   */
+  async updateMe(ctx) {
+    const input = ctx.request.body;
+
+    try {
+      await validateProfileUpdateInput(input);
+    } catch (err) {
+      return ctx.badRequest('ValidationError', err);
+    }
+
+    const updatedUser = await strapi.plugins['users-permissions'].services.user.edit(ctx.state.user.id, input);
+
+    ctx.body = sanitizeUser(updatedUser);
+  },
+};
