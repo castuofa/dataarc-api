0773a5d43ee5cfd9dd938573c8810319681bab3a
Tim Sexton <tlsexto@gmail.com>
2020-12-03 16:18:24 -0600

    Fixed auto email results for search generation, add search to cron

--- api/search/services/search.js
+++ api/search/services/search.js
@@ -7,63 +7,88 @@ module.exports = {
   exportResults: async () => {
     let searches = await strapi.query('search').find({ process: 1 });
     if (searches.length) log('debug', `Found ${searches.length} search job(s)`);
-    searches.forEach(async search => {
+    searches.forEach(async (search) => {
       let start = Date.now();
 
       let dir = `${strapi.dir}/public/export/`;
       let filename = `${search.id}.zip`;
 
-      if (fs.existsSync(dir+filename)) {
+      if (fs.existsSync(dir + filename)) {
         log('debug', `Export already exists for ${search.id}`);
-        strapi.query('search').update({ id: search.id }, { process: 0, filename: filename });
+        strapi
+          .query('search')
+          .update({ id: search.id }, { process: 0, filename: filename });
         return;
       }
       if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
       log('debug', `Generating search export for ${search.id}`);
-      const results = await strapi.services['query'].exportResults(search.filters);
+      const results = await strapi.services['query'].exportResults(
+        search.filters
+      );
 
+      let daysExpire = 30;
       let expires = new Date();
-      expires.setDate(expires.getDate() + 30);
+      expires.setDate(expires.getDate() + daysExpire);
 
       const output = fs.createWriteStream(`${dir}${filename}`);
       const archive = archiver('zip');
 
       output.on('close', async () => {
-        await strapi.query('search').update({ id: search.id }, { process: 0, processed: Date.now(), expires: expires, filename: filename });
-        log('debug', `Search results generated, ${archive.pointer()} total bytes`, start);
+        await strapi
+          .query('search')
+          .update(
+            { id: search.id },
+            {
+              process: 0,
+              processed: Date.now(),
+              expires: expires,
+              filename: filename,
+            }
+          );
+        log(
+          'debug',
+          `Search results generated, ${archive.pointer()} total bytes`,
+          start
+        );
 
         // send email notification
         await strapi.plugins['email'].services.email.send({
           to: search.user.email,
-          from: 'no-reply@data-arc.org',
           subject: 'DataARC Search Results',
-          html: `<h1>DataARC</h1><p>Your search results have been successfully generated. You can download the file from your profile or by clicking the link below.</p><p>Download Results: <a href="https://api.data-arc.org${dir}${filename}">https://api.data-arc.org${dir}${filename}</a></p>`,
+          html: `<h1>DataARC</h1><p>Your search results have been successfully generated. You can download the file from your profile or by clicking the link below. These results will expire and automatically be removed in ${daysExpire} days.</p><p>Download Results: <a href="https://api.data-arc.org${dir}${filename}">https://api.data-arc.org${dir}${filename}</a></p>`,
         });
       });
       output.on('error', (err) => {
-        log('error', err)
+        log('error', err);
       });
 
       archive.pipe(output);
-      archive.append(JSON.stringify(results, null, 2), { name: 'results.json' });
+      archive.append(JSON.stringify(results, null, 2), {
+        name: 'results.json',
+      });
       archive.finalize();
     });
   },
 
   // remove expired searches
   removeExpired: async () => {
-    let searches = await strapi.query('search').find({ expires_lt: Date.now() });
-    if (searches.length) log('debug', `Found ${searches.length} expired search(es)`);
+    let searches = await strapi
+      .query('search')
+      .find({ expires_lt: Date.now() });
+    if (searches.length)
+      log('debug', `Found ${searches.length} expired search(es)`);
     else return;
-    searches.forEach(search => {
+    searches.forEach((search) => {
       let dir = `${strapi.dir}/public/export/`;
       fs.unlink(`${dir}${search.filename}`, (err) => {
         log('debug', `Export removed for ${search.id}`);
-        strapi.query('search').update({ id: search.id }, { expires: null, filename: null });
+        strapi
+          .query('search')
+          .update({ id: search.id }, { expires: null, filename: null });
       });
     });
-  }
+  },
 };
 
 // simple log function with time calculation
--- config/functions/cron.js
+++ config/functions/cron.js
@@ -1,35 +1,32 @@
 'use strict';
 
-/**
- * Cron config that gives you an opportunity
- * to run scheduled jobs.
- *
- * The cron format consists of:
- * [SECOND (optional)] [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK]
- *
- * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#cron-tasks
- */
+let running = false;
 
 module.exports = {
-  '*/1 * * * *': () => {
-    console.log('1 minute later');
+  '*/1 * * * *': async () => {
+    // make sure only one instance is running to avoid conflicts
+    if (!running) {
+      running = true;
+
+      // remove expired searches
+      await strapi.services['search'].removeExpired();
+
+      // process search first to get results they expect
+      await strapi.services['search'].exportResults();
+
+      // process concept map
+      // await strapi.services['concept-map'].
+      // await strapi.services['concept-map'].process();
+
+      // // process datasets that are queued
+      // await strapi.services['dataset'].process();
+
+      // refresh datasets that are queued
+      // await strapi.services['dataset'].refresh();
+
+      // // refresh combinators
+      // await strapi.services['combinator'].refresh();
+    }
+    running = false;
   },
-
-  // check if concept-map needs to be processed
-  // check if concept map needs to be activated
-  // check if dataset needs to be processed
-  // check if dataset needs refreshed
-  // check if combinator needs refreshed
-  // check if search results need to be generated
-  // check if search results need to be deleted
-
-
-  /**
-   * Simple example.
-   * Every monday at 1am.
-   */
-  // '0 1 * * 1': () => {
-  //
-  // }
-
 };
